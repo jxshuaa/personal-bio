d8fa08f159cf2a123f4ead6663a99165d0e3ab63
jxshuaa <domainmanagement94@gmail.com>
2025-02-10 22:41:45 +0100

    added syphon.cc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,16 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="nav-links">
         <a href="https://github.com/jxshuaa" target="_blank" rel="noopener noreferrer">
-          Projects
+          projects
         </a>
         <a href="https://e-z.bio/jxsh" target="_blank" rel="noopener noreferrer">
-          My e-z.bio
+          my e-z.bio
         </a>
         <a href="https://discord.gg/UWFH2sEfCW" target="_blank" rel="noopener noreferrer">
-          Carbon Softworks Discord
+          carbon softworks
+        </a>
+        <a href="https://discord.gg/atkWHGTpNs" target="_blank" rel="noopener noreferrer">
+          syphon.cc
         </a>
       </div>
     </nav>
